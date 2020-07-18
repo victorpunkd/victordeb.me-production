201d8b509d63551e4f113d06748b357c417922fd
Victor Deb <vctrdb@gmail.com>
2020-07-19 01:04:01 +0530

    Live version, built with npm run build

---
+++ precache-manifest.a2c0eced6afe08e36e0279612cf0fc9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfa4696159aebd73b179",
+    "url": "/static/css/main.914b17b5.chunk.css"
+  },
+  {
+    "revision": "bfa4696159aebd73b179",
+    "url": "/static/js/main.bfa46961.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "34044f0e858b07508383",
+    "url": "/static/js/2.34044f0e.chunk.js"
+  },
+  {
+    "revision": "08f4d8d2926a421efa85e57452d75215",
+    "url": "/static/media/icon.08f4d8d2.png"
+  },
+  {
+    "revision": "c6d9a7fb0326ee2a380776392522b7cf",
+    "url": "/index.html"
+  }
+];
